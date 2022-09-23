ac90562330ed47c48311add45e557e7e072a073a
Code Sandbox <hello@codesandbox.io>
2022-09-23 16:45:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b1515ab91b61d08f13aacacded0697a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170c79928cd715057594bdf9c25cc55c",
+    "url": "https://santhoshmoolemane.github.io/csb-uf5i4o/index.html"
+  },
+  {
+    "revision": "ddbd16a452efc26b35f4",
+    "url": "https://santhoshmoolemane.github.io/csb-uf5i4o/static/css/main.f443e78c.chunk.css"
+  },
+  {
+    "revision": "202b3f290735fc2fa0c7",
+    "url": "https://santhoshmoolemane.github.io/csb-uf5i4o/static/js/2.a86d1960.chunk.js"
+  },
+  {
+    "revision": "ddbd16a452efc26b35f4",
+    "url": "https://santhoshmoolemane.github.io/csb-uf5i4o/static/js/main.d9d62944.chunk.js"
+  },
+  {
+    "revision": "039698fa7335ed8d4cfe",
+    "url": "https://santhoshmoolemane.github.io/csb-uf5i4o/static/js/runtime~main.0d4b334e.js"
+  }
+]);
